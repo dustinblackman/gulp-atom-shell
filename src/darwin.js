d30d3d3418600fbaaee9a67427ee8855a732ec90
Dustin Blackman <dustinblackman@users.noreply.github.com>
2015-05-09 15:34:32 -0400

    Remove version and license file

--- src/darwin.js
+++ src/darwin.js
@@ -99,12 +99,27 @@ function renameApp(opts) {
 	});
 }
 
+function removeFiles() {
+	var pass = es.through();
+	var arr = ['version', 'LICENSE'];
+
+	var src = pass.pipe(es.mapSync(function(f) {
+		if (arr.indexOf(f.relative) === -1) {
+			return f;
+		}
+
+	}));
+
+	return es.duplex(pass, src);
+}
+
 exports.patch = function(opts) {
 	var pass = es.through();
 
 	var src = pass
 		.pipe(removeDefaultApp())
 		.pipe(patchIcon(opts))
+		.pipe(removeFiles())
 		.pipe(patchInfoPlist(opts))
 		.pipe(renameApp(opts));
 
